14fe2f994ce85fd440665b09d88598567e211afd
Lucas Frare <31972242+frare@users.noreply.github.com>
2024-01-09 22:28:15 -0300

    Adicionada movimentação AEstrela de inimigos

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using Pathfinding;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float healthMax;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float moveAcceleration;
+    
+    [SerializeField] private Seeker seeker;
+    [SerializeField] private AIPath aiPath;
+
+    private float health;
+
+
+
+    private void Awake()
+    {
+        health = healthMax;
+        aiPath.maxAcceleration = moveAcceleration;
+        aiPath.maxSpeed = moveSpeed;
+    }
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+    }
+
+    [ContextMenu("Test Movement")]
+    private void TestMovement()
+    {
+        aiPath.destination = new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
     }
 }
